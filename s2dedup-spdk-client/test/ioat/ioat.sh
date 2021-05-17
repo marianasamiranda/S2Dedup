7a8f4fd7fdaf82006dd6539f327b68b1db8b836a
user Grupo de Sistemas Distribuidos <gsd@cloud129.cluster.lsd.di.uminho.pt>
2021-05-17 08:03:01 +0100

    S2Dedup's Client Final Version

--- s2dedup-spdk-client/test/ioat/ioat.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-testdir=$(readlink -f $(dirname $0))
-rootdir=$(readlink -f $testdir/../..)
-source $rootdir/test/common/autotest_common.sh
-
-run_test "ioat_perf" $rootdir/examples/ioat/perf/ioat_perf -t 1
-
-run_test "ioat_verify" $rootdir/examples/ioat/verify/verify -t 1
